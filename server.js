78c4dcdc75ee1ea645c53f8a37112c13a8b66198
Combi2k2 <cumbee2k2@gmail.com>
2022-04-26 14:06:53 +0200

    Not really the first commit because we did a lot of work before init the repo

---
+++ server.js
@@ -0,0 +1,8 @@
+// All we need
+let express = require('express');
+let app = express();
+
+// Serve files from the static directory
+app.use(express.static('./src'));
+app.listen(5000, () => console.log('Server running on PORT 5000'));
+console.log("Please go to http://localhost:3000/index.html to see the magic :D");
