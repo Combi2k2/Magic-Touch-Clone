703d2f1263f62f154a0c774a2c967f7ebdf9d333
Combi2k2 <cumbee2k2@gmail.com>
2022-04-28 18:18:02 +0200

    Normalize the name of sketching function (display -> render)

--- src/utils/explosion.js
+++ src/utils/explosion.js
@@ -2,7 +2,7 @@ const img_explosion = new Image();
 img_explosion.src = "images/explode_in_sky.png";
 img_explosion.onload = console.log("Explosion Loaded");
 
-function display_sprite_explosion(explosion_position, idx_explosion = 0)    {
+function render_explosion(explosion_position, idx_explosion = 0)    {
     let pos_x = explosion_position[0];
     let pos_y = explosion_position[1];
     
@@ -23,9 +23,6 @@ function display_sprite_explosion(explosion_position, idx_explosion = 0)    {
         missle_width,
         missle_height   // destination
     );
-    // setTimeout(() => {
-    //     display_sprite_explosion(explosion_position, idx_explosion + 1);
-    // }, 16);
 }
 
 //  when the missile hits the ground, you lose and a mushroom explosion will happen.
@@ -37,12 +34,12 @@ img_mushroom_explosion.onload = console.log("Mushroom Explosion Loaded");
 //  console.log(img_mushroom_explosion.naturalWidth);   //  1556
 //  console.log(img_mushroom_explosion.naturalHeight);  //  971
 
-function display_mushroom_explosion(idx_explosion = 0)  {   // display mushroom explosion at the middle bottom of the canvas
+function render_mushroom_explosion(idx_explosion = 0)   {   // display mushroom explosion at the middle bottom of the canvas
     let w = 318;    // width
     let h = 224;    // height
 
-    if (idx_explosion < 5)  h = 75;
-    if (idx_explosion > 24) return; // there is 25 explosion images - return after
+    if (idx_explosion < 5)
+        h = 75; //  the first row of the sprited mushroom explosion is trimmed
 
     //  scale of explosion depend on the window
     const explosion_width  = game_canvas.width * 0.5;
@@ -52,20 +49,14 @@ function display_mushroom_explosion(idx_explosion = 0)  {   // display mushroom 
     const pos_x = game_canvas.width / 2 - explosion_width / 2;
     const pos_y = game_canvas.height - explosion_height;
     
-    game_ctx.clearRect(0, 0, game_canvas.width, game_canvas.height);
-    
     // the (x,y)-offset of the current image
     const offset_x = (idx_explosion % 5) * w;
     const offset_y = Math.max(Math.floor(idx_explosion / 5) * 224 - 224 + 75, 0);
 
     game_ctx.drawImage(img_mushroom_explosion,
         offset_x, offset_y, w, h,   // source
-        pos_x, pos_y,
+        pos_x, pos_y - explosion_height,
         explosion_width,
         explosion_height    // destination
     );
-    setTimeout(function()   {
-        display_mushroom_explosion(idx_explosion + 1);
-    }, 100);
 }
-//  display_mushroom_explosion([100, 400], 0);
