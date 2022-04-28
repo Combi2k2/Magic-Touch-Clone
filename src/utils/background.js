e50f240ce5b70aeb03c21e63590c11aa1ec9aa50
Combi2k2 <cumbee2k2@gmail.com>
2022-04-28 18:39:13 +0200

    Add the function how to render the background image

--- src/utils/background.js
+++ src/utils/background.js
@@ -1,7 +1,7 @@
 const grass_layer = new Image();
 grass_layer.src = "images/grass.png";
 grass_layer.onload = function() {
-    console.log("Grassed Loaded");
+    console.log("Grass Loaded");
 };
 
 function render_Grass() {
@@ -17,4 +17,17 @@ function render_Grass() {
             i * w_grass, game_canvas.height - h_grass, w_grass, h_grass //  destination
         );
 
+}
+
+const sky_layer = new Image();
+sky_layer.src = "images/sky.png";
+sky_layer.onload = function()   {
+    console.log("Sky loaded");
+}
+
+function render_Sky()   {
+    game_ctx.drawImage(sky_layer,
+        0, 0,
+        game_canvas.width,
+        game_canvas.height);
 }
