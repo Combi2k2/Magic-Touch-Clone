69878ea4add523b4ef788333000369bc002e4185
Combi2k2 <cumbee2k2@gmail.com>
2022-04-28 13:33:02 +0200

    Nothing happened with this file

--- src/canvas_render.js
+++ src/canvas_render.js
@@ -1,2 +1,5 @@
 const game_canvas = document.getElementById('game-canvas');
 const game_ctx = game_canvas.getContext('2d');
+
+game_canvas.width  = window.innerWidth  * 0.75;
+game_canvas.height = window.innerHeight * 0.95;
