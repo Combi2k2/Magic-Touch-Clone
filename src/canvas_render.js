5aca891cb1403e30e6340bd7dc360888cdf3bbaa
Combi2k2 <cumbee2k2@gmail.com>
2022-04-29 15:15:16 +0200

    create responsive effect for the game canva

--- src/canvas_render.js
+++ src/canvas_render.js
@@ -1,5 +1,8 @@
 const game_canvas = document.getElementById('game-canvas');
 const game_ctx = game_canvas.getContext('2d');
 
-game_canvas.width  = window.innerWidth  * 0.75;
-game_canvas.height = window.innerHeight * 0.95;
+function game_canvas_setup()    {
+    game_canvas.width  = window.innerWidth  * 0.75;
+    game_canvas.height = window.innerHeight * 0.95;
+}
+game_canvas_setup();
