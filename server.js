7368528028a5560f5d176dcc5027ad4a58a923e2
Combi2k2 <cumbee2k2@gmail.com>
2022-05-01 18:40:26 +0200

    Not much things happened

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // All we need
 let express = require('express');
 let app = express();
+let PORT_default = 3000;
+let PORT = PORT_default || process.env.PORT;
 
 // Serve files from the static directory
 app.use(express.static('./src'));
-app.listen(3000, () => console.log('Server running on PORT 3000'));
-console.log("Please go to http://localhost:3000/index.html to see the magic :D");
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+console.log( `Please go to http://localhost:${PORT}/index.html to see the magic :D`);
